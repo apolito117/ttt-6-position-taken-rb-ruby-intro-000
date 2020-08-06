7d73249bf87959afa421501168efa430ba1b1531
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-08-06 03:40:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,11 +4,11 @@
 def position_taken?(board, index)
 
 taken = nil
-  if (board[position] ==  " " || board[position] == "" || board[position] == nil)
+  # if (board[position] ==  " " || board[position] == "" || board[position] == nil)
   if (board[index] ==  " " || board[index] == "" || board[index] == nil)
     taken = false
   else
     taken = true
   end
   taken
-end
+# end
