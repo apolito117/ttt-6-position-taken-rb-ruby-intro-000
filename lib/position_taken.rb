051130ce5719d90f37fd38b69b4eeec3f375f36d
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-08-06 03:36:59 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+position_taken?(board, index)
+taken = nil
+  if (board[position] ==  " " || board[position] == "" || board[position] == nil)
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    taken = false
+  else
+    taken = true
+  end
+  taken
