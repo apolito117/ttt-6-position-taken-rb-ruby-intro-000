2eb44c5af429c0ff565ce1dcadacad8e5b80ec3e
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-08-06 03:38:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 # code your #position_taken? method here!
-def position_taken?(board, position)
+
   # basic solution:
 def position_taken?(board, index)
+
 taken = nil
   if (board[position] ==  " " || board[position] == "" || board[position] == nil)
   if (board[index] ==  " " || board[index] == "" || board[index] == nil)
